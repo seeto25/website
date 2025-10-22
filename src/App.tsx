d8a8e1f8ba883bce52a2ab5162eb65d2f070fbb5
seeto25 <shpath.seeto@hotmail.com>
2025-10-22 10:47:17 +0200

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -322,8 +322,8 @@ function App() {
                       onClick={() => setCurrentScreenshot(index)}
                       className={`w-2.5 h-2.5 rounded-full transition-all duration-300 ${
                         index === currentScreenshot
-                          ? 'bg-brand-green w-8'
-                          : 'bg-brand-dark hover:bg-brand-green/50'
+                          ? 'bg-brand-grey w-8'
+                          : 'bg-brand-light hover:bg-brand-green/50'
                       }`}
                     />
                   ))}
