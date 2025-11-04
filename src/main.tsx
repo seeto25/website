7a9bd3fe539aa0059c698ae25af7ddf51fdf2a1f
seeto25 <shpath.seeto@hotmail.com>
2025-11-04 10:55:19 +0100

    Updated package-lock.json

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home.tsx';
 import Privacy from './pages/Privacy.tsx';
-import Legalnotice from './pages/Legalnotice.tsx';
+import Terms from './pages/Terms.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
@@ -12,7 +12,7 @@ createRoot(document.getElementById('root')!).render(
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/privacy" element={<Privacy />} />
-        <Route path="/legalnotice" element={<Legalnotice />} />
+        <Route path="/terms" element={<Terms />} />
       </Routes>
     </Router>
   </StrictMode>
