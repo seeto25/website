d45917b0c7622bd222feb96741f41e5435cd21a0
seeto25 <shpath.seeto@hotmail.com>
2026-03-28 17:30:53 +0100

    Updated Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -91,7 +91,6 @@ export default function Home() {
             <div className="w-12 h-6 bg-gray-500 rounded-b-full shadow-lg border-3 border-gray-600 relative">
               <div className="absolute inset-x-2 top-0.5 h-0.5 bg-gray-700 rounded-full"></div>
             </div>
-          </div>
 
             <div className="w-2.5 bg-brand-dark rounded-sm shadow-lg" style={{ height: '80px' }}>
               <div className="w-full h-1.5 bg-gray-700 rounded-t-sm"></div>
