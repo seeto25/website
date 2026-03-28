3f66f41474e2aa1390bbec7c2f97468fc232e2bf
seeto25 <shpath.seeto@hotmail.com>
2026-03-28 16:55:41 +0100

    Updated Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -64,7 +64,7 @@ export default function Home() {
                 clipPath: 'polygon(15% 0%, 85% 0%, 100% 15%, 100% 85%, 85% 100%, 15% 100%, 0% 85%, 0% 15%)',
                 minHeight: '90px',
                 outline: '4px solid white',
-                outlineOffset: '-18px',
+                outlineOffset: '-15px',
               }}
               aria-label="Zur App"
             >
